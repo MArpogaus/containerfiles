a21be2dad9db626432795ab1b97af0bf0ca93c2b
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2025-08-10 11:00:20 +0000

    fix: use /usr/etc to alter 'default config'

--- silverblue-main-hardened/build_files/build.sh
+++ silverblue-main-hardened/build_files/build.sh
@@ -5,25 +5,44 @@ set -ouex pipefail
 dnf5 install -y zsh
 
 ### Add public cosign key
+cd /tmp
+
 # Create a registry configuration file:
-cat > /etc/containers/registries.d/ghcr.io-marpogaus.yaml << EOF
+cat > /usr/etc/containers/registries.d/ghcr.io-marpogaus.yaml << EOF
 docker:
   ghcr.io/marpogaus:
     use-sigstore-attachments: true
 EOF
 
 # Download and install the public key:
-curl -o /etc/pki/containers/marpogaus-cosign.pub https://raw.githubusercontent.com/marpogaus/containerfiles/main/cosign.pub
+curl -o /usr/etc/pki/containers/marpogaus-cosign.pub https://raw.githubusercontent.com/marpogaus/containerfiles/main/cosign.pub
 
 # Update container policy to allow signed images from this repository
-podman image trust set -f /etc/pki/containers/marpogaus-cosign.pub --type sigstoreSigned ghcr.io/marpogaus
+POLICY_FILE="/usr/etc/containers/policy.json"
+
+jq --arg image_registry "ghcr.io/marpogaus" \
+   --arg image_registry_key "marpogaus-cosign" \
+   '.transports.docker |= 
+    { $image_registry: [
+        {
+            "type": "sigstoreSigned",
+            "keyPath": ("/usr/etc/pki/containers/" + $image_registry_key + ".pub"),
+            "signedIdentity": {
+                "type": "matchRepository"
+            }
+        }
+    ] } + .' "${POLICY_FILE}" > POLICY.tmp
+
+cp POLICY.tmp /usr/etc/containers/policy.json
+cp POLICY.tmp /etc/containers/policy.json
+rm POLICY.tmp
 
 ### Enable systemd-homed
+# Build and install SELinux custom policy
 TMP_DIR=/tmp/homed-selinux
 git clone https://github.com/richiedaze/homed-selinux $TMP_DIR
-
-# Build and install SELinux custom policy
 cd $TMP_DIR
+
 make -f /usr/share/selinux/devel/Makefile homed.pp
 semodule --install=homed.pp
 

edc55556ae9cc7dc3f00345ba104e492e30c7fb0
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2025-08-10 09:42:09 +0000

    feat: add public cosign key for images from this repo

--- silverblue-main-hardened/build_files/build.sh
+++ silverblue-main-hardened/build_files/build.sh
@@ -4,6 +4,20 @@ set -ouex pipefail
 ### Install packages
 dnf5 install -y zsh
 
+### Add public cosign key
+# Create a registry configuration file:
+cat > /etc/containers/registries.d/ghcr.io-marpogaus.yaml << EOF
+docker:
+  ghcr.io/marpogaus:
+    use-sigstore-attachments: true
+EOF
+
+# Download and install the public key:
+curl -o /etc/pki/containers/marpogaus-cosign.pub https://raw.githubusercontent.com/marpogaus/containerfiles/main/cosign.pub
+
+# Update container policy to allow signed images from this repository
+podman image trust set -f /etc/pki/containers/marpogaus-cosign.pub --type sigstoreSigned ghcr.io/marpogaus
+
 ### Enable systemd-homed
 TMP_DIR=/tmp/homed-selinux
 git clone https://github.com/richiedaze/homed-selinux $TMP_DIR

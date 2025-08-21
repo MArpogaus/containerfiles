bfdfa7e37454dabaf902b98fb3a26ff5454d5b10
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2025-08-21 10:57:57 +0200

    fix: add testing repo for pdf2svg

--- context/alpine/latex-build.sh
+++ context/alpine/latex-build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -ouex pipefail
 
+# add testing repo
+sudo tee -a /etc/apk/repositories <<< "@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing"
+
 # Update package database
 apk update
 
 # Install LaTeX and related packages
 apk add --no-cache \
 	texlive-most \
-	pdf2svg \
-	zathura \
+	pdf2svg@testing \
 	zathura-pdf-poppler
 
 # Clean package cache

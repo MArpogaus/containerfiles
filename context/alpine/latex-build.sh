d5631de094cc349f15263997fd459bf579489b72
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2025-08-21 11:55:15 +0200

    fix: add missing fonts

--- context/alpine/latex-build.sh
+++ context/alpine/latex-build.sh
@@ -2,7 +2,7 @@
 set -ouex pipefail
 
 # add testing repo
-sudo tee -a /etc/apk/repositories <<< "@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing"
+sudo tee -a /etc/apk/repositories <<<"@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing"
 
 # Update package database
 apk update
@@ -10,6 +10,7 @@ apk update
 # Install LaTeX and related packages
 apk add --no-cache \
 	texlive-most \
+	texmf-dist-fontsextra \
 	pdf2svg@testing \
 	zathura-pdf-poppler
 

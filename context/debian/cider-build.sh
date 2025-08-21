0cba4f4972ba6b939c2bce7329f57f1895774b3d
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2025-08-21 14:29:45 +0200

    fix: use wget instead of curl to retrieve apt key

--- context/debian/cider-build.sh
+++ context/debian/cider-build.sh
@@ -2,7 +2,7 @@
 set -ouex pipefail
 
 # Import the GPG key
-curl -fsSL https://repo.cider.sh/APT-GPG-KEY | gpg --dearmor -o /usr/share/keyrings/cider-archive-keyring.gpg
+wget -qO - https://repo.cider.sh/APT-GPG-KEY | gpg --dearmor -o /usr/share/keyrings/cider-archive-keyring.gpg
 
 # Add the repository to sources list
 echo "deb [signed-by=/usr/share/keyrings/cider-archive-keyring.gpg] https://repo.cider.sh/apt stable main" | tee /etc/apt/sources.list.d/cider.list

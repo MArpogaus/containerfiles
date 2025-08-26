4c5063574f2cc474059d6e07c685c230d6134535
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2025-08-26 10:59:17 +0200

    feat: add fedora based emacs container

---
+++ context/fedora/emacs-build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -ouex pipefail
+
+# Update package database
+dnf makecache
+
+# Install packages
+dnf install -y \
+	bat \
+	cmake \
+	ctags \
+	delta \
+	direnv \
+	emacs-pgtk \
+	enchant2 \
+	enchant2-devel \
+	fd \
+	fzf \
+	git \
+	graphviz \
+	htop \
+	hunspell \
+	hunspell-en \
+	ImageMagick \
+	libtool \
+	make \
+	pandoc \
+	parallel \
+	uv \
+	ripgrep \
+	shellcheck \
+	shfmt \
+	tmux \
+	tree \
+	unzip \
+	vim \
+	zathura-pdf-poppler \
+	zip
+
+# Clean package cache
+dnf clean all
